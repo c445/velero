ef983191a4abfa5a608ce5582958bc6cc7166cef
Wenkai Yin(尹文开) <yinw@vmware.com>
2023-11-10 10:16:47 +0800

    Skip syncing the backup which doesn't contain backup metadata
    Skip syncing the backup which doesn't contain backup metadata
    
    Fixes #6849
    
    Signed-off-by: Wenkai Yin(尹文开) <yinw@vmware.com>

--- pkg/controller/backup_sync_controller.go
+++ pkg/controller/backup_sync_controller.go
@@ -211,6 +211,16 @@ func (c *backupSyncController) run() {
 			log = log.WithField("backup", backupName)
 			log.Info("Attempting to sync backup into cluster")
 
+			exist, err := backupStore.BackupExists(location.Spec.ObjectStorage.Bucket, backupName)
+			if err != nil {
+				log.WithError(errors.WithStack(err)).Error("Error checking backup exist from backup store")
+				continue
+			}
+			if !exist {
+				log.Debugf("backup %s doesn't exist in backup store, skip", backupName)
+				continue
+			}
+
 			backup, err := backupStore.GetBackupMetadata(backupName)
 			if err != nil {
 				log.WithError(errors.WithStack(err)).Error("Error getting backup metadata from backup store")
